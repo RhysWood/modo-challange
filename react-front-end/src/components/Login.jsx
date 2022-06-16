befe6c3f4ebb4710af21daf6eefa6088a01a4403
RhysWood <rhyswoody@gmail.com>
2022-06-16 13:42:59 -0700

    bug fix

--- react-front-end/src/components/Login.jsx
+++ react-front-end/src/components/Login.jsx
@@ -1,113 +1,118 @@
 import { useRef, useState, useEffect, useContext } from "react";
 import { Navigate } from "react-router-dom";
 import AuthContext from "../context/AuthProvider";
-import './login.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./login.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import axios from "../api/axios"; 
-const LOGIN_URL = '/auth';
+import axios from "../api/axios";
+const LOGIN_URL = "/auth";
 
 export default function Login() {
-    //global state
-    const { setAuth } = useContext(AuthContext);
-    const userRef = useRef();
-    const errRef = useRef();
+  //global state
+  const { setAuth } = useContext(AuthContext);
+  const userRef = useRef();
+  const errRef = useRef();
 
-    const [user, setUser] = useState('');
-    const [pwd, setPwd] = useState('');
-    const [errMsg, setErrMsg] = useState('');
-    const [success, setSuccess] = useState(false);
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
 
-    //set focus on input
-    useEffect(() => {
-        userRef.current.focus();
-    }, [])
+  //set focus on input
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
 
-    //if user changes anything, remove err msg
-    useEffect(() => {
-        setErrMsg('');
-    }, [user, pwd])
+  //if user changes anything, remove err msg
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        try {
-            const response = await axios.post(LOGIN_URL,
-                JSON.stringify({ user, pwd }),
-                {
-                    headers: { 'Content-Type': 'application/json' },
-                    withCredentials: true
-                }
-            );
-            console.log(JSON.stringify(response?.data));
-            //console.log(JSON.stringify(response));
-            const accessToken = response?.data?.accessToken;
-            
-            setAuth({ user, pwd, accessToken });
-            setUser('');
-            setPwd('');
-            setSuccess(true);
-            if (success){<Navigate to="/vehicles"/>}
-
-        } catch (err) {
-            console.log(err);
-            if (!err?.response) {
-                setErrMsg('No Server Response');
-            } else if (err.response?.status === 400) {
-                setErrMsg('Missing Username or Password');
-            } else if (err.response?.status === 401) {
-                setErrMsg('Unauthorized');
-            } else {
-                setErrMsg('Login Failed');
-            }
-            errRef.current.focus();
+    try {
+      const response = await axios.post(
+        LOGIN_URL,
+        JSON.stringify({ user, pwd }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
         }
-    }
+      );
+      console.log(JSON.stringify(response?.data));
+      //console.log(JSON.stringify(response));
+      const accessToken = response?.data?.accessToken;
 
-    return(
-        <div className="parent">
-            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-        <div className="child">
+      setAuth({ user, pwd, accessToken });
+      setUser("");
+      setPwd("");
+      setSuccess(true);
+      if (success) {
+        <Navigate to="/vehicles" />;
+      }
+    } catch (err) {
+      console.log(err);
+      if (!err?.response) {
+        setErrMsg("No Server Response");
+      } else if (err.response?.status === 400) {
+        setErrMsg("Missing Username or Password");
+      } else if (err.response?.status === 401) {
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg("Login Failed");
+      }
+      errRef.current.focus();
+    }
+  };
+
+  return (
+    <div className="parent">
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+      <div className="child">
         <form onSubmit={handleSubmit}>
-        <div className="input-section">
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            id="email"
-            ref={userRef}
-            autoComplete="off"
-            className="form-control"
-            placeholder="Enter email"
-            onChange={(e) => setUser(e.target.value)}
-            value={user}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            id="password"
-            ref={userRef}
-            onChange={(e) => setPwd(e.target.value)}
-            value={pwd}
-            className="form-control"
-            placeholder="Enter password"
-            required
-          />
-        </div>
-        </div>
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Log in
-          </button>
-        </div>
-      </form>
+          <div className="input-section">
+            <div className="mb-3">
+              <label>Email address</label>
+              <input
+                type="email"
+                id="email"
+                ref={userRef}
+                autoComplete="off"
+                className="form-control"
+                placeholder="Enter email"
+                onChange={(e) => setUser(e.target.value)}
+                value={user}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label>Password</label>
+              <input
+                type="password"
+                id="password"
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                className="form-control"
+                placeholder="Enter password"
+                required
+              />
+            </div>
+          </div>
+          <div className="d-grid">
+            <button type="submit" className="btn btn-primary">
+              Log in
+            </button>
+          </div>
+        </form>
       </div>
-      {success && (
-          <Navigate to="/vehicles" />
-        )}
-        </div>
-    )
+      {success && <Navigate to="/vehicles" />}
+    </div>
+  );
 }
