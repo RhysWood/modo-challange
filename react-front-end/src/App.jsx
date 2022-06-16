ee49f902ee95fc4dda663b99c7b118dcc88d7c91
RhysWood <rhyswoody@gmail.com>
2022-06-15 21:18:50 -0700

    add react router dom

--- react-front-end/src/App.jsx
+++ react-front-end/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import './App.css';
+import Login from "./components/Login";
 
 function App() {
   return (
-    <p>
-      Cleanedup React App
-    </p>
+    <>
+      <BrowserRouter>
+         <Routes>
+         <Route path="/login" element={<Login />} />
+         </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
