2e7036666cef4bddfa53c81dc4feba26e1865cdd
RhysWood <rhyswoody@gmail.com>
2022-06-15 19:21:43 -0700

    add seeds

---
+++ express-back-end/db/seeds/01_vehicles.sql
@@ -0,0 +1,7 @@
+INSERT INTO vehicles (make, model, year, color, location_id, location_description) VALUES
+('Honda', 'Civic', 2009, 'silver', 4, 'Downtown Vancouver - Modo Office'),
+('Ford', 'Focus', 2015, 'navy', 7, 'Burnaby - Metrotown Towers'),
+('Volvo', 'XC70', 2005, 'blue', 2, 'Downtown Vancouver - Coal Harbor'),
+('Subaru', 'Outback', 2007, 'black', 4, 'Downtown Vancouver - Modo Office'),
+('Volkswagon', 'Jetta', 2012, 'silver', 7, 'Burnaby - Metrotown Towers'),
+('Rivian', 'R1T', 2022, 'yellow', 2, 'Downtown Vancouver - Coal Harbor')
---
+++ express-back-end/db/seeds/02_users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (email, password, timestamp) VALUES
+('test@example.com', 'hello', '2022-06-15 19:32'),
+('test2@example.com', 'hello2', '2022-06-15 19:35')
